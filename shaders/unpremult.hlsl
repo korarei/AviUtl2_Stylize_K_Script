6ceff80bf372d6540163d5f7d1f125e6ab3adc1f
korarei <132204522+korarei@users.noreply.github.com>
2025-07-24 02:29:45 +0900

    Feature: Add Unpremultiply script

---
+++ shaders/unpremult.hlsl
@@ -0,0 +1,12 @@
+Texture2D texture0 : register(t0);
+
+struct PS_INPUT {
+    float4 pos : SV_Position;
+    float2 uv : TEXCOORD0;
+};
+
+float4 unpremult(PS_INPUT input) : SV_Target {
+    float4 tex = texture0.Load(int3(input.pos.xy, 0));
+    float alpha = max(max(tex.r, tex.g), tex.b);
+    return float4(tex.rgb, alpha);
+}
