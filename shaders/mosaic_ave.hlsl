7468f2c50bda17636e8d3ee9ff8761773f71235c
korarei <132204522+korarei@users.noreply.github.com>
2025-08-14 06:56:06 +0900

    Feat: Add mosaic effect

---
+++ shaders/mosaic_ave.hlsl
@@ -0,0 +1,54 @@
+RWTexture2D<half4> tex_out : register(u0);
+Texture2D tex_in : register(t0);
+cbuffer params : register(b0) {
+    float2 blocks;
+    float2 res;
+    float total_groups;
+    float total_blocks;
+};
+
+struct CS_INPUT {
+    uint3 gid : SV_GroupID;
+    uint3 gtid : SV_GroupThreadID;
+    uint gidx : SV_GroupIndex;
+};
+
+groupshared float4 col_buf[64];
+groupshared uint cnt_buf[64];
+
+[numthreads(8, 8, 1)]
+void mosaic_map(CS_INPUT input) {
+    for (uint i = input.gid.x; i < uint(total_blocks); i += uint(total_groups)) {
+        uint map_w = uint(blocks.x);
+        uint2 block_idx = uint2(i % map_w, i / map_w);
+        float2 block_size = res * rcp(blocks);
+        uint2 st = uint2(block_idx * block_size);
+        uint2 ed = min(uint2((block_idx + 1) * block_size), uint2(res));
+
+        float4 l_col = float4(0.0, 0.0, 0.0, 0.0);
+        uint l_cnt = 0;
+        for (uint y = st.y + input.gtid.y; y < ed.y; y += 8) {
+            for (uint x = st.x + input.gtid.x; x < ed.x; x += 8) {
+                l_col += tex_in[uint2(x, y)];
+                l_cnt++;
+            }
+        }
+        col_buf[input.gidx] = l_col;
+        cnt_buf[input.gidx] = l_cnt;
+
+        GroupMemoryBarrierWithGroupSync();
+        for (uint s = 32; s > 0; s >>= 1) {
+            if (input.gidx < s) {
+                col_buf[input.gidx] += col_buf[input.gidx + s];
+                cnt_buf[input.gidx] += cnt_buf[input.gidx + s];
+            }
+            GroupMemoryBarrierWithGroupSync();
+        }
+
+        if (input.gidx == 0) {
+            float4 col = col_buf[0];
+            uint cnt = cnt_buf[0];
+            tex_out[block_idx] = col * rcp(max(cnt, 1));
+        }
+    }
+}
---
+++ shaders/mosaic_point.hlsl
@@ -0,0 +1,17 @@
+Texture2D tex : register(t0);
+cbuffer params : register(b0) {
+    float2 blocks;
+    float2 res;
+};
+
+struct PS_INPUT {
+    float4 pos : SV_Position;
+    float2 uv : TEXCOORD0;
+};
+
+float4 mosaic_point(PS_INPUT input) : SV_Target {
+    float2 block_size = res * rcp(blocks);
+    int2 block_idx = int2(input.pos.xy * rcp(block_size));
+    int2 pos = int2((block_idx + 0.5) * block_size);
+    return tex.Load(int3(clamp(pos, 0, int2(res) - 1), 0));
+}
