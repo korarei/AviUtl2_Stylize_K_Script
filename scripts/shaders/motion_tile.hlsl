2fc1e951a6842457bbefe0738bda334483a90d41
Korarei <132204522+korarei@users.noreply.github.com>
2025-12-04 05:37:19 +0900

    Refactor: Update Motion Tile script

--- scripts/shaders/motion_tile.hlsl
+++ scripts/shaders/motion_tile.hlsl
@@ -1,39 +1,40 @@
 Texture2D src : register(t0);
-SamplerState src_smp : register(s0);
+SamplerState smp : register(s0);
 cbuffer params : register(b0) {
-    float2 tex_size;
+    column_major float2x2 rm;
     float2 center;
-    float2 output_gain;
-    float2 tile_gain;
+    float2 output;
+    float2 scale;
     float2 mirror;
+    float2 phase_dir;
     float phase;
-    float h_shift;
+    float aspect;
 };
 
-struct PS_INPUT {
+static const float eps = 1.0e-4;
+
+struct PS_Input {
     float4 pos : SV_Position;
-    float2 uv : TEXCOORD0;
+    float2 uv : TEXCOORD;
 };
 
-float4 tiler(float2 pos) {
-    int2 idx = int2(floor(pos));
-    float2 parity = float2(idx & 1);
-    float2 uv = pos - idx;
-    float2 coord = lerp(uv, 1.0 - uv, mirror * parity);
+float4 wrap(float2 pos) {
+    const float2 idx = floor(pos);
+    const float2 odd = frac(idx * 0.5) * 2.0;
+    const float2 uv = pos - idx;
 
-    float2 e = 0.5 * rcp(tex_size);
-    return src.Sample(src_smp, clamp(coord, e, 1.0 - e));
+    return src.Sample(smp, lerp(uv, 1.0 - uv, mirror * odd));
 }
 
-float4 motion_tile(PS_INPUT input) : SV_Target {
-    float2 pos = input.uv * output_gain;
-    pos -= 0.5 * output_gain + center;
-    pos /= max(tile_gain, 1.0e-5);
+float4 motion_tile(PS_Input input) : SV_Target {
+    float2 pos = mad(input.uv, output, -center);
+    pos.y *= aspect;
+    pos = mul(rm, pos) * rcp(max(scale, eps));
+    pos.y *= rcp(aspect);
     pos += 0.5;
 
-    float2 parity = float2(int2(floor(pos)) & 1);
-    float2 shift_dir = parity.yx * float2(h_shift, 1.0 - h_shift);
-    pos -= shift_dir * phase;
+    const float2 odd = frac(floor(pos) * 0.5) * 2.0;
+    pos -= phase * odd.yx * phase_dir;
 
-    return tiler(pos);
+    return wrap(pos);
 }
